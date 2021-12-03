63be25c050142f3d25f4b25f584568e37844841a
duyên huynh <dh18062000@gmail.com>
2021-12-03 16:16:59 +0700

    add res

--- js/slider.js
+++ js/slider.js
@@ -11,12 +11,40 @@ function carousel() {
       dots[i].classList.remove("scale-up-hor-left")
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
+    if (myIndex > x.length) {myIndex = 1}  
+    
     x[myIndex-1].style.display = "block";  
+    
     dots[myIndex-1].classList.add("active-dot");
     dots[myIndex-1].classList.add("scale-up-hor-left")
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    // setTimeout(carousel, 2000); // Change image every 2 seconds
   }
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    
+  }
+var timer=window.setInterval(()=>carousel(),2000)
+document.querySelector("#next-icon").onclick=function(){
+  
+  window.clearInterval(timer);
+ 
+ 
+
+}
+document.querySelector("#pre-icon").onclick=function(){
+  clearTimeout(carousel);
+}
 
 
 document.querySelector("#discount-mem").onclick=function(){
@@ -31,14 +59,14 @@ document.querySelectorAll(".out-popup").forEach(btn=>btn.onclick=function(){
 })
 
 
-document.querySelector(".hamburger-menu").onclick=function(){
-  if( this.classList.contains("animate")){
-      this.classList.remove("animate")
-      document.querySelector(".content").classList.remove("animate-col")
-  }else{
-      this.classList.add("animate");
-      document.querySelector(".user").classList.add("hidden")
-      document.querySelector(".content").classList.add("animate-col")
-  }
+// document.querySelector(".hamburger-menu").onclick=function(){
+//   if( this.classList.contains("animate")){
+//       this.classList.remove("animate")
+//       document.querySelector(".content").classList.remove("animate-col")
+//   }else{
+//       this.classList.add("animate");
+//       document.querySelector(".user").classList.add("hidden")
+//       document.querySelector(".content").classList.add("animate-col")
+//   }
   
-}
+// }
